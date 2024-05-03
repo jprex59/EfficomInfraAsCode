5795a6897b8f8a36af7115a5d84620068bcb33cd
jprex <jean59paris@orange.fr>
2024-05-03 10:50:49 +0200

    ansible install apache sur la machine local

--- COURS1/random_string.tf
+++
@@ -1,6 +0,0 @@
-resource "random_string" "lower" {
-  length           = 4
-  lower = true
-  upper = false
-  special = false
-}
--- COURS1/variables.tf
+++
@@ -1,7 +0,0 @@
-variable "project_id" {
-  default     = "terra-effi-126383"  // Spécifiez l'ID du projet ici
-}
-
-variable "region" {
-  default     = "europe-west1"
-}
